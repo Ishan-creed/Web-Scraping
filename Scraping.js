3ad089448ec57abd6096c58e0241a247a8b6f3be
Ishan Verma <83359975+Ishan-creed@users.noreply.github.com>
2023-09-18 13:03:13 +0530

    Update Scraping.js

--- Scraping.js
+++ Scraping.js
@@ -1,34 +1,43 @@
 const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
 
+// Function to scrape reviews from TripAdvisor
 async function scrapeTripAdvisorReviews() {
   const browser = await puppeteer.launch({
     headless: true,
     args: ['--no-sandbox', '--disable-setuid-sandbox', '--window-size=1920,1080'],
   });
   const page = await browser.newPage();
+
+  // Currently Try using it for resturants, hotel link might not work... wokring on that
+  
   const tripAdvisorUrl = 'https://www.tripadvisor.in/Restaurant_Review-g295424-d18856876-Reviews-Kinara_by_Vikas_Khanna-Dubai_Emirate_of_Dubai.html';
 
   let currentPage = 1;
   const tripAdvisorData = [];
 
   while (true) {
+    // Construct the URL for each page of reviews
     const pageUrl = `${tripAdvisorUrl}-or${(currentPage - 1) * 10}`;
     await page.goto(pageUrl);
     await page.waitForSelector('.review-container');
 
+    // Extract and format review data from the current page
     const reviews = await page.evaluate(() => {
       const results = [];
       const items = document.querySelectorAll('.review-container');
 
       items.forEach((item) => {
+        // Extract and parse the rating
         let ratingElement = item.querySelector('.ui_bubble_rating').getAttribute('class');
         let integer = ratingElement.replace(/[^0-9]/g, '');
         let parsedRating = parseInt(integer) / 10;
 
+        // Extract and format date of visit
         let dateOfVisitElement = item.querySelector('.prw_rup.prw_reviews_stay_date_hsx').innerText;
         let parsedDateOfVisit = dateOfVisitElement.replace('Date of visit:', '').trim();
 
+        // Push review data into results array
         results.push({
           name: item.querySelector('.info_text.pointer_cursor div').innerText,
           rating: parsedRating,
@@ -42,25 +51,30 @@ async function scrapeTripAdvisorReviews() {
       return results;
     });
 
+    // Append the reviews from the current page to the main data array
     tripAdvisorData.push(...reviews);
 
+    // Log reviews for the current page
     console.log(`TripAdvisor Page ${currentPage} Reviews: `, reviews);
 
+    // Check if there is a next page, if not, break the loop
     const nextButton = await page.$('.nav.next');
     if (!nextButton) {
       break;
     }
 
+    // Click the next page button and wait for a short duration
     await nextButton.click();
     await page.waitForTimeout(1000);
     currentPage++;
   }
 
+  // Close the browser and return the scraped TripAdvisor data
   await browser.close();
-
   return tripAdvisorData;
 }
 
+// Function to scrape reviews from Google Travel
 async function scrapeGoogleTravelReviews() {
   const browser = await puppeteer.launch({ headless: true });
   const page = await browser.newPage();
@@ -70,26 +84,31 @@ async function scrapeGoogleTravelReviews() {
   let previousReviewCount = 0;
   let currentReviewCount = 0;
 
+  // Navigate to the Google Travel URL
   await page.goto(googleTravelUrl);
 
+  // Infinite scroll to load more reviews
   while (true) {
     await page.evaluate(() => {
       window.scrollTo(0, document.body.scrollHeight);
     });
     await page.waitForTimeout(1000);
 
+    // Extract reviews using Cheerio
     const pageContent = await page.content();
     const $ = cheerio.load(pageContent);
 
     currentReviewCount = $('.Svr5cf.bKhjM').length;
 
+    // If no new reviews are loaded, exit the loop
     if (currentReviewCount === previousReviewCount) {
-      break; // No new reviews loaded, exit the loop
+      break;
     }
 
     previousReviewCount = currentReviewCount;
   }
 
+  // Extract and format review data using Cheerio
   const pageContent = await page.content();
   const $ = cheerio.load(pageContent);
 
@@ -110,13 +129,13 @@ async function scrapeGoogleTravelReviews() {
     });
   });
 
+  // Log the scraped Google Travel data and close the browser
   console.log(data);
-
   await browser.close();
-
   return data;
 }
 
+// Main function to orchestrate scraping both sources
 async function main() {
   const tripAdvisorReviews = await scrapeTripAdvisorReviews();
   console.log('TripAdvisor Reviews:', tripAdvisorReviews);
@@ -125,4 +144,5 @@ async function main() {
   console.log('Google Travel Reviews:', googleTravelReviews);
 }
 
+// Call the main function to start the scraping process
 main();
